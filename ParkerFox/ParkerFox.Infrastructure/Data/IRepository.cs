4628a36f80115647a2e5711c1c55d9193626b36b
sebastian <sabkent@hotmail.com>
2012-06-13 18:13:24 +0100

    more repo poo

--- ParkerFox/ParkerFox.Infrastructure/Data/IRepository.cs
+++ ParkerFox/ParkerFox.Infrastructure/Data/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,8 +13,8 @@ namespace ParkerFox.Infrastructure.Data
     /// <typeparam name="T"></typeparam>
     public interface IRepository<T>
     {
-        IUnitOfWork Add(T entity);
-        IUnitOfWork Remove(T entity);        
-        IQueryable<T> Query();
+        void Add(T entity);
+        void Remove(T entity);
+        IQueryable<T> Query(Expression<Func<T, object>> expression);
     }
 }
--- ParkerFox/ParkerFox.Infrastructure/Repository/Repository.cs
+++ ParkerFox/ParkerFox.Infrastructure/Repository/Repository.cs
@@ -2,37 +2,41 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using NHibernate;
 using ParkerFox.Infrastructure.Data;
 
 namespace ParkerFox.Infrastructure.Repository
 {
     /// <summary>
     /// http://blog.bobcravens.com/2010/06/the-repository-pattern-with-linq-to-fluent-nhibernate-and-mysql/
+    /// http://www.storminajar.net/2010/08/30/letting-your-unitofwork-repositories-support-multiple-frameworks/
     /// </summary>    
-    public class Repository<T> : IRepository<T>
+    public class Repository<T> : IRepository<T> where T : class
     {
-        private readonly IUnitOfWork _UnitOfWork;
+        private readonly ISession _Session;
 
-        public Repository(IUnitOfWork unitOfWork)
+        public Repository(ISession session)
         {
-            _UnitOfWork = unitOfWork;
+            _Session = session;
         }
 
-        IUnitOfWork IRepository<T>.Add(T entity)
+        void IRepository<T>.Add(T entity)
         {
-            throw new NotImplementedException();
+            _Session.SaveOrUpdate(entity);            
         }
 
-        public IUnitOfWork Remove(T entity)
+        public void Remove(T entity)
         {
-            throw new NotImplementedException();
+            _Session.Delete(entity);
         }
 
-        public IQueryable<T> Query()
+        public IQueryable<T> Query(Expression<Func<T, object>> expression) 
         {
             throw new NotImplementedException();
+            //return _Session.QueryOver<T>().Fetch(expression);
         }
     }
 }
