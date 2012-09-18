90983e5b53dc6dc6658a8aa29f7d9c299cacbee0
sebastian kent <sabkent@hotmail.com>
2012-09-18 17:31:17 +0100

    new console host for WCF services

---
+++ ParkerFox/ParkerFox.Services.Host/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkerFox.Services.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Server listening...");
+            Console.ReadLine();
+        }
+    }
+}
