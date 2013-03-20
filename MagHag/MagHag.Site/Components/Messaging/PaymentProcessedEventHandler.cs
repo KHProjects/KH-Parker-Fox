fd081807f320ea3864321d7129c395a47fff2f1c
Sebastian <sabkent@hotmail.com>
2013-03-20 12:58:24 +0000

    fuck off

---
+++ MagHag/MagHag.Site/Components/Messaging/PaymentProcessedEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MagHag.Billing.Messaging.Events;
+using NServiceBus;
+
+namespace MagHag.Site.Components.Messaging
+{
+    public class PaymentProcessedEventHandler : IHandleMessages<PaymentProcessedEvent>
+    {
+        public void Handle(PaymentProcessedEvent message)
+        {
+            bool pass = true;
+        }
+    }
+}
